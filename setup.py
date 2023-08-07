108702ca996e154b99d9bc940fc520b19eb1c5e4
DevinDeSilva <devin.18@cse.mrt.ac.lk>
2023-08-07 14:35:10 +0530

    Initialized Repo

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'CodeBase of important Functions'
+LONG_DESCRIPTION = 'Contains Pytorch/ TensorFlow algorithms I have found in a single module'
+
+# Setting up
+setup(
+        name="DYCodeBase", 
+        version=VERSION,
+        author="Devin De Silva",
+        author_email="devin@irononetech.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["tensorflow==2.10.*"], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'codebase'],
+        classifiers= [
+            "Development Status :: 3 - Pre-Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3.9",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
