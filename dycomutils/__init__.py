78ea62e1230b564e586844ebb9080be6faed4d9e
Devin De Silva <desild@rpi.edu>
2025-10-18 01:00:29 -0400

    The configDict class was added

--- dycomutils/__init__.py
+++ dycomutils/__init__.py
@@ -1,9 +1,11 @@
 from . import serialization
 from . import concurrancy
 from . import grouping
+from . import config
 
 __all__ = [
     'serialization',
     'concurrancy',
     'grouping',
+    'config',
 ]
